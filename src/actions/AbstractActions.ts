1200c76ab220e345ef6433c6041f19cfe7eb8701
Shearerbeard <shearerbeard@wavmxr.com>
2015-06-24 21:19:58 -0600

    moving abstract actions to only declarations to cut back on LoC

--- src/actions/AbstractActions.ts
+++ src/actions/AbstractActions.ts
@@ -2,5 +2,5 @@ import Alt = require("alt");
 
 export class AbstractActions implements AltJS.ActionsClass {
   constructor(alt:Alt){}
-  dispatch( ...payload:Array<any>) {}
+  dispatch: ( ...payload:Array<any>) => void;
 }
